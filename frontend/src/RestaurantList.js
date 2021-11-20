ede2aca94672718caf04c0fbec7d78f0c35e2ad9
Stephen Alarcón <slj212885@gmail.com>
2021-11-20 00:14:12 -0500

    Filtrando columnas y mejorando el Frontend

--- frontend/src/RestaurantList.js
+++ frontend/src/RestaurantList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faTrashAlt, faCheck, faTimes, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faTrashAlt, faCheck, faTimes, faPlus, faStar, faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import {faStar as farStar} from '@fortawesome/free-regular-svg-icons'
+
 import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 import RestaurantService from './RestaurantService';
@@ -22,7 +24,9 @@ class RestaurantList extends Component {
                 rating: 0,
                 visited: false
               },
-            direction: '',
+            direction: 'descending',
+            input: '',
+            restaurantsBackup: []
         };
     }
     modalInsertar = () => {
@@ -33,7 +37,7 @@ class RestaurantList extends Component {
     componentDidMount() {
         var self = this;
         restaurantsService.getRestaurants().then(function (result) {
-            self.setState({ restaurants: result })
+            self.setState({ restaurants: result, restaurantsBackup: result })
         });
     }
 
@@ -97,7 +101,7 @@ class RestaurantList extends Component {
     }
 
     sortByColumn(column){
-        if (this.state.direction == '' || this.state.direction == 'descending'){
+        if (this.state.direction == 'descending'){
             this.setState({restaurant: this.state.restaurants.sort((a, b) => (a[column] > b[column] ? 1 : a[column] < b[column] ? -1 : 0))});
             this.setState({direction: 'ascending'});
         }else if(this.state.direction == 'ascending'){
@@ -106,8 +110,50 @@ class RestaurantList extends Component {
         }
     }
 
+    onChangeHandler(e){
+        this.setState({
+            input: e.target.value,
+        })
+    }
+
+    filter(event){
+        console.log(event.target.value)
+        const data = this.state.restaurantsBackup
+        var text = event.target.value
+
+        const newData = data.filter(function(item){
+          const itemDataName = item.name.toUpperCase()
+          const itemDataLocation = item.location.toUpperCase()
+          const itemDataKindFood = item.kind_food.toUpperCase()
+          const itemDataVisited = item.visited.toString().toUpperCase()
+          const itemData = itemDataName+" "+itemDataLocation+" "+itemDataKindFood+" "+item.rating+""+itemDataVisited
+          const textData = text.toUpperCase()
+          return itemData.indexOf(textData) > -1
+        });
+        this.setState({
+            restaurants: newData,
+            input: text,
+        })
+    }
+   
+
     render() {
+        var self = this;
+
         const activeItem = this.state.activeItem;
+        
+        const renderStars = stars =>{
+            let content = [];
+            const starsWhite = 5 - stars;
+            for(let i=0; i<stars; i++){
+                content.push(<FontAwesomeIcon icon={faStar} className="text-warning"/>)
+            }
+            for(let i=0; i<starsWhite; i++){
+                content.push(<FontAwesomeIcon icon={farStar} className="text-secondary"/>)
+            }
+            return content;
+        }
+        
         let button;
         if(activeItem.id == null){
             button = <button className="btn btn-success" onClick={() => this.handleCreate()}>Agregar</button>
@@ -119,32 +165,58 @@ class RestaurantList extends Component {
             <div className="customers--list">
                 <h1>Restaurantes</h1>
                 <button className="btn btn-success float-right mb-2" onClick={() => this.modalInsertar()}>Agregar Restaurante <FontAwesomeIcon className="ml-1" icon={faPlus} /></button>
+                <div className="form-row">
+                    <div className="col offset-8">
+                        <input className="form-control float-right" type="text" placeholder="Buscar..." value={this.state.text} onChange={(text) => this.filter(text)}/>
+                    </div>
+                </div>
                 <table className="table">
                     <thead key="thead">
-                        <tr >
-                            <th >#</th>
-                            <th onClick={() => this.sortByColumn('name')}><span style={{cursor: 'pointer'}}>Nombre</span></th>
-                            <th onClick={() => this.sortByColumn('location')}><span style={{cursor: 'pointer'}}>Ubicación</span></th>
-                            <th onClick={() => this.sortByColumn('kind_food')}><span style={{cursor: 'pointer'}}>Tipo de comida</span></th>
-                            <th onClick={() => this.sortByColumn('rating')}><span style={{cursor: 'pointer'}}>Calificación</span></th>
-                            <th onClick={() => this.sortByColumn('visited')}><span style={{cursor: 'pointer'}}>Visitado</span></th>
+                        <tr>
+                            <th>#</th>
+                            <th onClick={() => this.sortByColumn('name')}>
+                                <span>Nombre<FontAwesomeIcon className="ml-2"  
+                                icon={this.state.direction == 'descending' ? faArrowDown: faArrowUp} />
+                                </span>
+                            </th>
+                            <th onClick={() => this.sortByColumn('location')}>
+                                <span>Ubicación<FontAwesomeIcon className="ml-2"  icon={this.state.direction == 'descending' ? faArrowDown: faArrowUp} />
+                                </span>
+                            </th>
+                            <th onClick={() => this.sortByColumn('kind_food')}>
+                                <span>Tipo de comida<FontAwesomeIcon className="ml-2"  icon={this.state.direction == 'descending' ? faArrowDown: faArrowUp} />
+                                </span>
+                            </th>
+                            <th onClick={() => this.sortByColumn('rating')}>
+                                <span>Calificación<FontAwesomeIcon className="ml-2"  icon={this.state.direction == 'descending' ? faArrowDown: faArrowUp} />
+                                </span>
+                            </th>
+                            <th onClick={() => this.sortByColumn('visited')}>
+                                <span>Visitado<FontAwesomeIcon className="ml-2"  icon={this.state.direction == 'descending' ? faArrowDown: faArrowUp} />
+                                </span>
+                            </th>
                             <th>Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
+                        
                         {this.state.restaurants.map((value, index) =>
                             <tr key={value.id}>
                                 <td>{index + 1}</td>
                                 <td>{value.name}</td>
                                 <td>{value.location}</td>
                                 <td>{value.kind_food}</td>
-                                <td>{value.rating}</td>
+                                <td>{renderStars(value.rating)}</td>
                                 <td>
                                     {value.visited ? <FontAwesomeIcon className="text-success" icon={faCheck} /> : <FontAwesomeIcon className="text-danger" icon={faTimes} />}
                                 </td>
                                 <td>
-                                    <button className="btn btn-warning mr-2" onClick={() => this.handleSelect(value)}><FontAwesomeIcon icon={faEdit} /></button>
-                                    <button className="btn btn-danger" onClick={(e) => this.handleDelete(value.id)}><FontAwesomeIcon icon={faTrashAlt} /></button>
+                                    <button className="btn btn-warning mr-2" onClick={() => this.handleSelect(value)}>
+                                        <FontAwesomeIcon icon={faEdit} />
+                                    </button>
+                                    <button className="btn btn-danger" onClick={(e) => this.handleDelete(value.id)}>
+                                        <FontAwesomeIcon icon={faTrashAlt} />
+                                    </button>
                                 </td>
                             </tr>)}
                     </tbody>
@@ -158,15 +230,20 @@ class RestaurantList extends Component {
                     <ModalBody>
                         <div className="form-group">
                             <label htmlFor="nombre">Nombre</label>
+
                             <input className="form-control" type="text" name="name" id="name" ref="name" placeholder="Ingrese el nombre del restaurante" onChange={this.handleChange} value={activeItem.name}/>
+
                             <label htmlFor="ubicacion">Ubicación</label>
                             <input className="form-control" type="text" name="location" id="location" ref="location" placeholder="Ingrese la ubicación del restaurante"onChange={this.handleChange} value={activeItem.location}/>
+
                             <label htmlFor="Tipo de comida">Tipo de comida</label>
                             <input className="form-control" type="text" name="kind_food" id="kind_food" ref="kind_food" placeholder="Ingrese el tipo de comida" onChange={this.handleChange} value={activeItem.kind_food}/>
+
                             <label htmlFor="calificacion">Calificación</label>
                             <input className="form-control" type="number" name="rating" id="rating" max="5" min="0" ref="rating" placeholder="Ingrese una calificación del 0 al 5" onChange={this.handleChange} value={activeItem.rating}/>
+
                             <label htmlFor="visitado" check>Visitado</label>
-                            <input className="ml-1" type="checkbox" name="visited" id="visited" ref="visited" placeholder="Enter Todo Title" onChange={this.handleChange} checked={activeItem.visited}/>
+                            <input className="ml-1" type="checkbox" name="visited" id="visited" ref="visited" onChange={this.handleChange} checked={activeItem.visited}/>
                         </div>
                     </ModalBody>
 
